a8dddaf7837591975da7d61fb158cc5139df552d
MarbiFox <benjamin.fernandez.m01@mail.pucv.cl>
2022-03-24 11:54:48 -0300

    Finalizando el Tutorial

--- HolaMundoC.c
+++
@@ -1,8 +0,0 @@
-//Tarea_2 Lab_3 Git
-
-#include <stdio.h>
-
-int main() {
-    printf("Hola Mundo");
-    return 0;
-}
---
+++ Tarea_2.c
@@ -0,0 +1,8 @@
+//Intento 2 para Lab 3
+
+#include <stdio.h>
+
+int main (){
+    printf("Hello World");
+    return 0;
+}
